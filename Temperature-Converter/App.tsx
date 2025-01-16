bfe72b800f0ce9a0d25c42f457dfab26f852cf0f
Nittala Naga Sreeram <nnsreeram@digisprint.com>
2025-01-16 15:49:19 +0530

    temperature-converter-mobile-application-done

--- Temperature-Converter/App.tsx
+++ Temperature-Converter/App.tsx
@@ -2,15 +2,25 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { s } from "./StyleSheets/TempConverterJSS.style";
 import { ImageBackground, Text, View } from "react-native";
 import { Input } from "./Components/Input";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { DisplayTemperature } from "./Components/DisplayTemperature";
-import { convertTemperatureTo, returnOppositUnit } from "./utils/TemperatureConversion";
+import { convertTemperatureTo, returnOppositUnit, isIceTemperature } from "./utils/TemperatureConversion";
 import { Button } from "./Components/ButtonConvert";
+import { UNITS } from "./Constants/ConstantsValues";
 
 export default function App() {
 
 const [temperatureDisplay, setTemperatureDisplay] = useState(0);
-const [currentUnit, setCurrentUnit] = useState("℃")
+const [currentUnit, setCurrentUnit] = useState(UNITS.celcius)
+const [curentBackGround, setCurrentBackGround] = useState(require('./assets/cold.png'));
+
+useEffect(()=>{
+  if(isIceTemperature(temperatureDisplay, currentUnit)){
+    setCurrentBackGround(require('./assets/cold.png'))
+  }else{
+    setCurrentBackGround(require('./assets/hot.png'))
+  }
+},[temperatureDisplay, currentUnit])
 
 function checkingForValidTemperatureInput(){
   if(isNaN(temperatureDisplay)){
@@ -21,7 +31,7 @@ function checkingForValidTemperatureInput(){
 }
 
   return (
-    <ImageBackground source={require('./assets/hot.png')} style={s.backgroundImg}>
+    <ImageBackground source={curentBackGround} style={s.backgroundImg}>
     <SafeAreaProvider>
       <SafeAreaView style={s.root}>
         <View style={s.workspace}>
--- Temperature-Converter/utils/TemperatureConversion.js
+++ Temperature-Converter/utils/TemperatureConversion.js
@@ -10,6 +10,16 @@ export function convertTemperatureTo(temperature, untiTo) {
   }
 }
 
+export function isIceTemperature(temperature, unit) {
+  if (unit === UNITS.celcius) {
+    return temperature <= 0;
+  } else if (unit === UNITS.fahrenheit) {
+    return temperature <= 32;
+  } else {
+    throw new Error("Invalid Unit");
+  }
+}
+
 export function returnOppositUnit(unit) {
   return unit === UNITS.celcius ? UNITS.fahrenheit : UNITS.celcius;
 }
